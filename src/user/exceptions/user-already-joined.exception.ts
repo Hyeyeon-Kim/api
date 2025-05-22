97a6d42a794eda281d8d379140ca127ea575a3b5
Hyeyeon-Kim <clscls2530@naver.com>
2025-05-22 22:10:22 +0900

    fix: already user exception

--- src/user/exceptions/user-already-joined.exception.ts
+++ src/user/exceptions/user-already-joined.exception.ts
@@ -1,7 +1,7 @@
 import { HttpStatus } from "@nestjs/common";
-import { CustomException } from "src/common/exception/custom-exception";
+import { ConflictException } from "src/common/exception/custom-exception";
 
-export class UserAlredyJoinedException extends CustomException {
+export class UserAlredyJoinedException extends ConflictException {
   constructor(id: string) {
     super(`이미 가입된 회원입니다 (${id}).`, HttpStatus.NOT_FOUND);
   }
